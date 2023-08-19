0722c7d0c802c8a125ea14889717b04f2b374679
deepakmajhi22 <98248994+deepakmajhi22@users.noreply.github.com>
2023-08-19 13:40:34 +0530

    Update wsgi.py

--- src/main/wsgi.py
+++ src/main/wsgi.py
@@ -6,7 +6,9 @@ It exposes the WSGI callable as a module-level variable named ``application``.
 For more information on this file, see
 https://docs.djangoproject.com/en/2.0/howto/deployment/wsgi/
 """
-
+"""
+application = get_wsgi_application()
+"""
 import os
 
 from django.core.wsgi import get_wsgi_application
@@ -14,3 +16,4 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "main.settings")
 
 application = get_wsgi_application()
+app = application
